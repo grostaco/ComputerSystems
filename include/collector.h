eb888b4533823e33893c4b605af97ef32c2e5c4c
grostaco <grostaco@gmail.com>
2022-03-15 16:55:47 +0700

    docs: add comments

--- include/collector.h
+++ include/collector.h
@@ -10,18 +10,48 @@ static inline void generic_cleanup(void *x) {
     free(*(void**)x);
 }
 
+/**
+ * @struct collector
+ * @brief Structure representing a collector process
+
+ */
 struct collector {
-    pid_t process_id;
-    int readfd;
-    int writefd;
-    int outfd;
+    pid_t process_id; // Collector's process ID
+    int readfd;       // Descriptor for the collector to read from
+    int writefd;      // Descriptor to write to the collector
+    int outfd;        // Descriptor to read from the collector
 };
 
+/**
+ * @brief Structure representing the result of collectors
+ * 
+ */
 struct collector_result {
-    pid_t process_id;
-    uint64_t sum;
+    pid_t process_id; // Collector's process ID
+    uint64_t sum;     // Summed value of all collector's input
 };
 
+/**
+ * @brief Create a new collector
+ * 
+ * @param expected required pushed elements until sum
+ * @return struct collector 
+ */
 struct collector collector_create(size_t expected);
+
+/**
+ * @brief Push a value to be summed to a collector
+ * 
+ * @param collector collector for the value to pushed onto
+ * @param val value to be pushed onto the collector
+ */
 void collector_push(struct collector *collector, uint32_t val);
+
+/**
+ * @brief Collect summed results from collectors
+ * 
+ * @param collectors collectors to be collected from 
+ * @param n collector count
+ * @return collected results 
+ */
 struct collector_result *collector_collect(struct collector* collectors, size_t n);
--- src/collector.c
+++ src/collector.c
@@ -7,18 +7,30 @@
 #include <sys/types.h>
 #include <sys/poll.h>
 
+/**
+ * @brief Create a new collector
+ * 
+ * @param expected required pushed elements until sum
+ * @return struct collector 
+ */
 struct collector collector_create(size_t expected) {
+    // Create a pipe for parent <-> child communication
     int pipefd[2];
     pipe(pipefd);
+
+    // Extra pipe for collector_collect. The write end is disregarded.
     int outfd[2];
     pipe(outfd);
 
+    // Begin forking
     pid_t pid = fork();
     
     if (pid > 0) {
+        // Return back to parent with the collector descriptor
         return (struct collector){.process_id = pid, .readfd = pipefd[0], .writefd = pipefd[1], .outfd = outfd[0]};
     }
 
+    // Begin receiving values and summing
     uint64_t sum = 0;
     uint32_t num;
 
@@ -28,34 +40,43 @@ struct collector collector_create(size_t expected) {
         --expected;
     }
 
+    // Once the expected quota is reached, write back to outfd 
     write(outfd[1], &sum, sizeof sum);
+
+    // Close the write end.
     close(outfd[1]);
     exit(0);
 }
 
 void collector_push(struct collector *collector, uint32_t val) {
+    // Nothing special. A nice wrapper to write to the end of the collector pipe.
     write(collector->writefd, &val, sizeof val);
 }
 
 // Consume every collectors and close their connections
 struct collector_result *collector_collect(struct collector* collectors, size_t n) {
+    // Allocate enough memory to contain every collected results
     struct collector_result *results = malloc(sizeof *results * n);
     struct pollfd *pfds = calloc(n, sizeof *pfds);
     int nfds = n;
 
+    // Watch for events when outfd is ready
     for (int i = 0 ; i < nfds ; ++i) {
         pfds[i].fd = collectors[i].outfd;
         pfds[i].events = POLLIN;
     }
 
+    // While there is a collector that's not collected
     while (n) {
         int fd;
 
+        // Poll until a collector is ready
         fd = poll(pfds, nfds, -1);
         if (fd == -1) {
             perror("poll");
         }
 
+        // Run through the list to find an available outfd to read from 
         for (int i = 0 ; i < nfds ; ++i) {
             if (pfds[i].revents & POLLIN) {
                 uint64_t sum;

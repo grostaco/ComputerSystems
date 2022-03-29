c40491f5e5f16743e3c7de13f5332e77e1adb704
grostaco <grostaco@gmail.com>
2022-03-29 11:05:57 +0700

    feat: add example output

--- docs/openmp_acc.md
+++ docs/openmp_acc.md
@@ -28,3 +28,27 @@ int main () {
 ```
 
 The program will read `N` random numbers from `/dev/urandom` then add them up through OpenMP's `loop` construct. After everything has been added, the program will print out all the accumulated read values.
+
+Possible output:
+```sh
+$ make clean run
+...
+./bin/out 
+Got number: 39424
+Got number: 29160
+Got number: 14283
+Got number: 1952
+Got number: 62085
+Got number: 40454
+Got number: 49583
+Got number: 59401
+Got number: 54781
+Got number: 12487
+Got number: 13307
+Got number: 17932
+Got number: 44972
+Got number: 19086
+Got number: 13907
+Got number: 28462
+Accumulated sum: 501276
+```
--- src/main.c
+++ src/main.c
@@ -1,64 +1,24 @@
-/*
- * To compile: gcc 00_race_condition.c -o 00_race_condition.o -lpthread
- */
-#include <pthread.h>
-#include <semaphore.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#define NITER 1000000
+#define N 16
 
-int cnt = 0;
-pthread_mutex_t lock;
+int main () {
+    int rd = open("/dev/urandom", O_RDONLY);
 
-void * Count(void __attribute__((unused)) *a)
-{
-    int i, tmp;
-    pthread_mutex_lock(&lock);
-    for(i = 0; i < NITER; i++)
-    {
-        tmp = cnt;      /* copy the global cnt locally */
-        tmp = tmp+1;    /* increment the local copy */
-        cnt = tmp;      /* store the local value into the global cnt */ 
-    }
-    pthread_mutex_unlock(&lock);
-    return NULL;
-}
+    uint16_t *numbers = calloc(N, sizeof *numbers);
+    uint64_t sum = 0;
+    read(rd, numbers, N * sizeof *numbers);
 
-int main(void)
-{
-    pthread_mutex_init(&lock, NULL);
-    pthread_t tid1, tid2;
-
-    if(pthread_create(&tid1, NULL, Count, NULL))
-    {
-      printf("\n ERROR creating thread 1");
-      exit(1);
+#pragma omp parallel for 
+    for (size_t i = 0 ; i < N ; ++i) {
+        // printf is usually locked.
+        printf("Got number: %d\n", numbers[i]);
+        sum += numbers[i];
     }
 
-    if(pthread_create(&tid2, NULL, Count, NULL))
-    {
-      printf("\n ERROR creating thread 2");
-      exit(1);
-    }
-
-    if(pthread_join(tid1, NULL))	/* wait for the thread 1 to finish */
-    {
-      printf("\n ERROR joining thread");
-      exit(1);
-    }
-
-    if(pthread_join(tid2, NULL))        /* wait for the thread 2 to finish */
-    {
-      printf("\n ERROR joining thread");
-      exit(1);
-    }
-
-    if (cnt < 2 * NITER) 
-        printf("\n BOOM! cnt is [%d], should be %d\n", cnt, 2*NITER);
-    else
-        printf("\n OK! cnt is [%d]\n", cnt);
-  
-    pthread_mutex_destroy(&lock);
-    pthread_exit(NULL);
+    printf("Accumulated sum: %ld\n", sum);
 }
